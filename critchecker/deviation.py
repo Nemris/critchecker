3b8497e68214860e0a5f6e065ec2797bbc15df6d
Nemris <nemris@protonmail.com>
2020-12-11 17:13:19 +0100

    Add deviation category extraction

--- critchecker/deviation.py
+++ critchecker/deviation.py
@@ -23,3 +23,25 @@ def extract_id(url: str) -> str:
         return re.search(pattern, url).group(1)
     except AttributeError as exception:
         raise ValueError(f"'{url}': invalid deviation URL") from exception
+
+
+def extract_category(url: str) -> str:
+    """
+    Extract the category from a deviation URL.
+
+    Args:
+        url: The URL to a deviation.
+
+    Returns:
+        The deviation category.
+
+    Raises:
+        ValueError: If no deviation category is found.
+    """
+
+    pattern = r"https://www\.deviantart\.com/[A-Za-z0-9\-]+/([a-z\-]+)/.+-\d+$"
+
+    try:
+        return re.search(pattern, url).group(1)
+    except AttributeError as exception:
+        raise ValueError(f"'{url}': invalid deviation URL") from exception
