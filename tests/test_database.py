c64a180d1257df132d14df71ad59566d8cec51a6
Nemris <nemris@protonmail.com>
2020-12-20 22:23:25 +0100

    Add strategy to generate database rows

---
+++ tests/test_database.py
@@ -0,0 +1,31 @@
+""" Tests for critchecker.database. """
+
+from hypothesis.strategies import composite
+from hypothesis.strategies import integers
+from hypothesis.strategies import none
+from hypothesis.strategies import text
+
+from critchecker import database
+
+
+# pylint: disable=no-value-for-parameter
+
+
+@composite
+def rows(draw):
+    """
+    Hypothesis strategy to generate dummy database Row()s.
+    """
+
+    row = {
+        "crit_parent_id": draw(integers(1)),
+        "crit_parent_type": draw(integers(1)),
+        "crit_id": draw(integers(1)),
+        "crit_posted_at": draw(text(min_size=1)),
+        "crit_edited_at": draw(text(min_size=1) | none()),
+        "crit_author": draw(text(min_size=1)),
+        "crit_words": draw(integers(1)),
+        "crit_url": draw(text(min_size=1))
+    }
+
+    return database.Row(**row)
