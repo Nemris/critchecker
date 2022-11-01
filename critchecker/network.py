9654ce44938a7c309adc969d21803d430a617302
Nemris <nemris@protonmail.com>
2022-11-01 21:10:15 +0100

    Specialize fetching logic

--- critchecker/network.py
+++ critchecker/network.py
@@ -1,6 +1,7 @@
 """ Functions that handle network requests. """
 
 import asyncio
+import json
 
 import aiohttp
 
@@ -19,6 +20,40 @@ class ConnectionTimedOutError(FetchingError):
 
 # Aliasing to avoid importing aiohttp in other modules to get sessions.
 Session = aiohttp.ClientSession
+
+
+async def fetch(url: str, session: Session, **kwargs: dict) -> dict:
+    """
+    Asynchronously download and return the data found at the URL.
+
+    Args:
+        url: The URL to download data from.
+        session: A session to use for requesting data.
+        **kwargs: Optional keyword arguments to pass to session.get().
+
+    Returns:
+        The response body.
+
+    Raises:
+        ConnectionTimedOutError: If the connection times out.
+        FetchingError: If a generic network error occurs.
+    """
+
+    try:
+        async with session.get(url, **kwargs) as response:
+            response.raise_for_status()
+            return await response.text()
+    except aiohttp.ClientResponseError as exception:
+        raise FetchingError(exception.message) from exception
+    except aiohttp.ServerTimeoutError as exception:
+        raise ConnectionTimedOutError(exception) from exception
+    except (
+        aiohttp.ClientConnectionError,
+        aiohttp.ClientError
+    ) as exception:
+        raise FetchingError(exception) from exception
+    except asyncio.TimeoutError as exception:
+        raise ConnectionTimedOutError(exception) from exception
 
 
 async def fetch_json(url: str, session: Session, **kwargs: dict) -> dict:
@@ -40,19 +75,8 @@ async def fetch_json(url: str, session: Session, **kwargs: dict) -> dict:
     """
 
     try:
-        async with session.get(url, **kwargs) as response:
-            response.raise_for_status()
-            return await response.json()
-    except aiohttp.ContentTypeError as exception:
-        raise BadJSONError(exception.message) from exception
-    except aiohttp.ClientResponseError as exception:
-        raise FetchingError(exception.message) from exception
-    except aiohttp.ServerTimeoutError as exception:
-        raise ConnectionTimedOutError(exception) from exception
-    except (
-        aiohttp.ClientConnectionError,
-        aiohttp.ClientError
-    ) as exception:
-        raise FetchingError(exception) from exception
-    except asyncio.TimeoutError as exception:
-        raise ConnectionTimedOutError(exception) from exception
+        return json.loads(
+            await fetch(url, session, **kwargs)
+        )
+    except json.JSONDecodeError as exception:
+        raise BadJSONError("malformed JSON response") from exception
